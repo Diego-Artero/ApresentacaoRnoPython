2cbea1c89241adf7919c33bbc575a49c924c3969
Diego-Artero <diego.bernardo.artero@gmail.com>
2024-06-04 16:26:38 -0300

    Update mainusandomatplot.py

--- Grafos5/mainusandomatplot.py
+++ Grafos5/mainusandomatplot.py
@@ -1,4 +1,3 @@
-# Importando as bibliotecas necessárias
 import pandas as pd
 import numpy as np
 import networkx as nx
@@ -7,7 +6,7 @@ import matplotlib.pyplot as plt
 # Carregando o arquivo
 corpus = pd.read_excel('ArquivoRespostas.xlsx', header=None)
 
-# Remover vazios/espaços e substituir por NA
+# Removendo células vazias e substituindo por NA
 corpus.replace("", np.nan, inplace=True)
 
 # Transformando o corpus em uma lista de listas
@@ -15,14 +14,14 @@ corpus = corpus.values.tolist()
 
 # Ordenando usuários
 usuarios = sorted(set([item for sublist in corpus for item in sublist if pd.notna(item)]))
-usuario_indice = {usuario: i for i, usuario in enumerate(usuarios)}
+indice_usuario = {usuario: i for i, usuario in enumerate(usuarios)}
 
 # Matriz de incidência
 mtx_inc = np.zeros((len(usuarios), len(corpus)), dtype=int)
 for col, sublist in enumerate(corpus):
     for usuario in sublist:
         if pd.notna(usuario):
-            mtx_inc[usuario_indice[usuario], col] = 1
+            mtx_inc[indice_usuario[usuario], col] = 1
 
 # Matriz de similaridade
 mtx_sim = np.dot(mtx_inc, mtx_inc.T)
@@ -31,7 +30,7 @@ np.fill_diagonal(mtx_sim, 0)
 # Matriz de co-ocorrência
 mtx_co = np.zeros((len(usuarios), len(usuarios)), dtype=int)
 for sublist in corpus:
-    sublist_usuarios = [usuario_indice[usuario] for usuario in sublist if pd.notna(usuario)]
+    sublist_usuarios = [indice_usuario[usuario] for usuario in sublist if pd.notna(usuario)]
     for i in range(len(sublist_usuarios)):
         for j in range(i + 1, len(sublist_usuarios)):
             mtx_co[sublist_usuarios[i], sublist_usuarios[j]] += 1
@@ -47,15 +46,15 @@ for usuario in usuarios:
     G_incidencia.add_node(usuario, bipartite=0)
 
 # Adicionando nós para eventos
-events = list(range(len(corpus)))
-for event in events:
-    G_incidencia.add_node(f'Event_{event}', bipartite=1)
+eventos = list(range(len(corpus)))
+for evento in eventos:
+    G_incidencia.add_node(f'Evento_{evento}', bipartite=1)
 
 # Adicionando arestas entre usuários e eventos
 for col, sublist in enumerate(corpus):
     for usuario in sublist:
         if pd.notna(usuario):
-            G_incidencia.add_edge(usuario, f'Event_{col}')
+            G_incidencia.add_edge(usuario, f'Evento_{col}')
 
 # Desenhando o grafo de incidência
 plt.figure(figsize=(12, 8))
@@ -73,8 +72,8 @@ for i in range(len(usuarios)):
 
 # Desenhando o grafo de similaridade
 plt.figure(figsize=(12, 8))
-weights = [G_similaridade[u][v]['weight'] for u, v in G_similaridade.edges()]
-nx.draw(G_similaridade, with_labels=True, node_size=700, node_color='skyblue', font_size=10, font_weight='bold', width=weights)
+pesos = [G_similaridade[u][v]['weight'] for u, v in G_similaridade.edges()]
+nx.draw(G_similaridade, with_labels=True, node_size=700, node_color='skyblue', font_size=10, font_weight='bold', width=pesos)
 plt.title('Grafo de Similaridade')
 plt.show()
 
@@ -87,27 +86,27 @@ for i in range(len(usuarios)):
 
 # Desenhando o grafo de coocorrência
 plt.figure(figsize=(12, 8))
-weights = [G_coocorrencia[u][v]['weight'] for u, v in G_coocorrencia.edges()]
-nx.draw(G_coocorrencia, with_labels=True, node_size=700, node_color='skyblue', font_size=10, font_weight='bold', width=weights)
+pesos = [G_coocorrencia[u][v]['weight'] for u, v in G_coocorrencia.edges()]
+nx.draw(G_coocorrencia, with_labels=True, node_size=700, node_color='skyblue', font_size=10, font_weight='bold', width=pesos)
 plt.title('Grafo de Coocorrência')
 plt.show()
 
 # Métricas topológicas
-def graph_metrics(graph, graph_name):
-    print(f"Métricas do grafo {graph_name}:")
-    print("Número de vértices:", graph.number_of_nodes())
-    print("Número de arestas:", graph.number_of_edges())
-    degrees = dict(graph.degree())
-    print("Graus de vértice:", degrees)
-    mean_degree = np.mean(list(degrees.values()))
-    print("Grau médio:", mean_degree)
-    if nx.is_weighted(graph):
-        weights = nx.get_edge_attributes(graph, 'weight')
-        mean_weight = np.mean(list(weights.values()))
-        print("Força de conectividade média:", mean_weight)
-    density = nx.density(graph)
-    print("Densidade da rede:", density)
-graph_metrics(G_incidencia, "de incidência")
-graph_metrics(G_similaridade, "de similaridade")
-graph_metrics(G_coocorrencia, "de co-ocorrência")
+def metricas_grafo(grafo, nome_grafo):
+    print(f"Métricas do grafo {nome_grafo}:")
+    print("Número de vértices:", grafo.number_of_nodes())
+    print("Número de arestas:", grafo.number_of_edges())
+    graus = dict(grafo.degree())
+    print("Graus de vértice:", graus)
+    grau_medio = np.mean(list(graus.values()))
+    print("Grau médio:", grau_medio)
+    if nx.is_weighted(grafo):
+        pesos = nx.get_edge_attributes(grafo, 'weight')
+        peso_medio = np.mean(list(pesos.values()))
+        print("Força de conectividade média:", peso_medio)
+    densidade = nx.density(grafo)
+    print("Densidade da rede:", densidade)
 
+metricas_grafo(G_incidencia, "de incidência")
+metricas_grafo(G_similaridade, "de similaridade")
+metricas_grafo(G_coocorrencia, "de co-ocorrência")
